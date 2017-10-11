84e5d24841b2683ac001a8b718bbf3dea11a1ef1
FIC <alandermm@hotmail.com>
2017-10-10 21:49:06 -0300

    criei um arquivo;

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace Aula5_2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            FileInfo fi = new FileInfo("Arquivo.txt");       
+            if (fi.Exists){
+                Console.WriteLine("Tamanho: " + fi.Length + " bytes");
+                Console.WriteLine("Extensão " + fi.Extension);
+                Console.WriteLine("Caminho: " + fi.FullName);
+            } else {            
+                Console.WriteLine("Arquivo não existe");    
+            }
+
+            StreamWriter sw = new StreamWriter("Arquivo.txt", true);
+            sw.WriteLine("Teste 1");
+            sw.WriteLine("Teste 2");
+            sw.WriteLine("_____________________________");
+            sw.Close();
+        }
+    }
+}
